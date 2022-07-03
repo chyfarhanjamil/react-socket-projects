beb9cc498106eab68184a976832d803295fcd441
Chowdhury Farhan Jamil <chy.farhan4@gmail.com>
2022-07-03 16:27:18 +0600

    right side (includes trend card) front-end complete

--- social-media/SocialMedia-Starter/src/components/RightSide/RightSide.jsx
+++ social-media/SocialMedia-Starter/src/components/RightSide/RightSide.jsx
@@ -1,6 +1,23 @@
 import React from "react";
 import "./rightSide.css";
+import Home from "../../img/home.png";
+import Notification from "../../img/noti.png";
+import Comment from "../../img/comment.png";
+import { UilSetting } from "@iconscout/react-unicons";
+import TrendCard from "../TrendCard/TrendCard";
+import { Button } from "@mantine/core";
 
 export default function RightSide() {
-  return <div>RightSide</div>;
+  return (
+    <div className="RightSide">
+      <div className="navIcons">
+        <img src={Home} alt="" />
+        <UilSetting />
+        <img src={Notification} alt="" />
+        <img src={Comment} alt="" />
+      </div>
+      <TrendCard />
+      <button className="button r-button">Share</button>
+    </div>
+  );
 }
